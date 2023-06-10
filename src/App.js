affdac931141f00c9bb9876ca11b2b66aad5c1fa
a-blaine <ai.blaine4@protonmail.com>
2023-06-10 19:45:55 +0100

    Added functionality for calculator

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <BmiCalculator />
+      <BmiCalculator defaultHeight={155} defaultWeight={85} />
       <footer>
         This app is <a href="/">open-sourced</a> on GitHub and hosted on Netlify
       </footer>
--- src/BmiCalculator.js
+++ src/BmiCalculator.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 
-export default function BmiCalculator() {
+export default function BmiCalculator({ defaultHeight, defaultWeight }) {
+  const [height, setHeight] = useState(defaultHeight);
+  const [weight, setWeight] = useState(defaultWeight);
+
+  const updateHeight = (event) => {
+    let newHeight = event.target.value;
+    setHeight(newHeight);
+  };
+
+  const updateWeight = (event) => {
+    let newWeight = event.target.value;
+    setWeight(newWeight);
+  };
+
+  const calculateResult = useMemo(() => {
+    let calculatedHeight = height / 100;
+    return (weight / (calculatedHeight * calculatedHeight)).toFixed(1);
+  }, [height, weight]);
+
   return (
     <div className="BmiCalculator">
       <h1>BMI Calculator</h1>
       <div className="container">
-        <p>Weight: kg </p>
-        <input type="range" />
-        <p>Height: cm</p>
-        <input type="range" />
+        <p>Weight: {weight} kg </p>
+        <input type="range" min={36} max={230} onChange={updateWeight} />
+        <p>Height: {height} cm</p>
+        <input type="range" min={155} max={220} onChange={updateHeight} />
         <div>
           <p>Your BMI is: </p>
-          <p className="bmi-result">20.5</p>
+          <p className="bmi-result">{calculateResult}</p>
         </div>
       </div>
     </div>
